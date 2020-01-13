c4f95ae297aa86d63598c0cdebc15f584f6da489
guowankeji <1378906626@qq.com>
2020-01-13 12:59:27 +0800

    init commit

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,61 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { observer, inject } from '@tarojs/mobx'
+
+import { AtNavBar } from 'taro-ui'
+
+import './index.scss'
+
+
+@inject('counterStore')
+@observer
+class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentWillReact () {
+    console.log('componentWillReact')
+  }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  increment = () => {
+    const { counterStore } = this.props
+    counterStore.increment()
+  }
+
+  decrement = () => {
+    const { counterStore } = this.props
+    counterStore.decrement()
+  }
+
+  incrementAsync = () => {
+    const { counterStore } = this.props
+    counterStore.incrementAsync()
+  }
+
+  render () {
+    const { counterStore: { counter } } = this.props
+    return (
+      <View className='index'>
+        <AtNavBar>首页</AtNavBar>
+        <Button onClick={this.increment}>+</Button>
+        <Button onClick={this.decrement}>-</Button>
+        <Button onClick={this.incrementAsync}>Add Async</Button>
+        <Text>{counter}</Text>
+      </View>
+    )
+  }
+}
+
+export default Index 
